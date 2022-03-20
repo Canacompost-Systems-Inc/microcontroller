7ed7d107220e00bbbdbb5c0d50a1f7df68cae7d2
Sohail Rajabi <srajabi@gmail.com>
2022-03-19 18:07:00 -0700

    CAN-87 initial work on MCU-IC protocol

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#ifdef UNIT_TEST
+    #include "ArduinoFake.h"
+#else
+    #include "Arduino.h"
+#endif
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
