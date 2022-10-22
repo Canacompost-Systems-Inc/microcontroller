a2d2f17b6c161044be8ad45e3778d43e24512b49
ksackvil <sackvillehiik@gmail.com>
2022-10-21 17:32:44 -0700

    hall sensor driver created

---
+++ src/Boards/YFS201/YFS201.hpp
@@ -0,0 +1,22 @@
+#ifndef YFS201_H
+#define YFS201_H
+
+#include "Sensor.hpp"
+#include "DFRobot_SHT40.h"
+
+
+class YFS201: public Sensor {
+ private:
+  float pulseFrequency;
+  Array<float> read();
+
+ public:
+  YFS201(char inBaseDID, unsigned long inPollingFrequency, int inPin):
+    Sensor(inBaseDID, inPollingFrequency, inPin) {};
+
+  void begin();
+
+  void pulse();
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,12 @@
 #include "Boards/SCD41/SCD41.hpp"
 #include "Boards/IPC10100/IPC10100.hpp"
 #include "Boards/DS18B20/DS18B20.hpp"
+#include "Boards/YFS201/YFS201.hpp"
 #include "ControlUnit/ControlUnit.hpp"
 
 // ----- CONSTANTS ----- //
 const unsigned long DEFAULT_POLLING_INTERVAL = 5000;
+const unsigned long FAST_POLLING_INTERVAL = 1000;
 
 // ----- OBJECTS ----- //
 // Actuator Objects
@@ -25,10 +27,15 @@ SHT40 	    sht40(0xC0, DEFAULT_POLLING_INTERVAL, -1);
 SCD41 	    scd41(0xC1, DEFAULT_POLLING_INTERVAL, -1);
 IPC10100 ipc10100(0xC2, DEFAULT_POLLING_INTERVAL, -1);
 DS18B20	  ds18b20(0xC3, DEFAULT_POLLING_INTERVAL, 2);
+YFS201	   yfs201(0xC4, FAST_POLLING_INTERVAL, 3);
 
 Array<Sensor*> sensors;
 Array<Actuator*> actuators;
 ControlUnit controller;
+
+void YSF201InterruptHandler() {
+	yfs201.pulse();
+}
 
 void setupActuators() {
   actuators.insert(&valve0);
@@ -39,12 +46,16 @@ void setupActuators() {
 }
 
 void setupSensors() {
-	sensors.insert(&sht40);
-	sensors.insert(&scd41);
+	// sensors.insert(&sht40);
+	// sensors.insert(&scd41);
+	sensors.insert(&yfs201);
 
 	for (int i = 0; i < sensors.getSize(); i++) {
 		sensors.read(i)->begin();
 	}
+
+	// Attach interrupts
+	attachInterrupt(digitalPinToInterrupt(yfs201.getPin()), YSF201InterruptHandler, RISING);
 
 	// Wait to allow for sensors to setup
 	delay(DEFAULT_POLLING_INTERVAL);
@@ -53,7 +64,7 @@ void setupSensors() {
 //  Runs every time serial connection is established OR when arduino is powered on.
 void setup() {
 	Serial.begin(9600);
-	setupActuators();
+	// setupActuators();
 	setupSensors();
 	controller.begin(actuators, sensors);
 }
