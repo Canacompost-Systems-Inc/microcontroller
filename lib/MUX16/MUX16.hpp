4262820f32b4fc62fb98874a2f460cac6926f5e8
ksackvil <sackvillehiik@gmail.com>
2023-01-11 09:35:50 -0800

    Cleanup MUX16 lib

--- lib/MUX16/MUX16.hpp
+++ lib/MUX16/MUX16.hpp
@@ -1,20 +1,36 @@
 #include <Arduino.h>
 
 
+// Tested with cd74HC4067 16-channel analog MUX, but logic should work for any MUX with 4 selector pins
 class MUX16 {
  private:
+  // States of the selector pins for each channel, indexed by selected channel number
   static const int LUT[16][4];
 
   int sigPin;
   int enablePin;
-  int selectorPin[4];
+  int selectorPin[4]; // [S3, S2, S1, S0]
 
  public:
   MUX16(int inSigPin, int inEnablePin, int inS3Pin, int inS2Pin, int inS1Pin, int inS0Pin):
   sigPin(inSigPin), enablePin(inEnablePin), selectorPin{inS3Pin, inS2Pin, inS1Pin, inS0Pin}  {};
 
   void begin();
+
+  /**
+   * Selects the channel to be read from the MUX16
+   * 
+   * @param channel int between 0 and 15 representing the respective channel
+   */
   void select(int channel);
+
+  /**
+   * Enables signal output from the MUX16
+   */
   void enable();
+
+  /**
+   * Disables signal output from the MUX16
+   */
   void disable();
 };
