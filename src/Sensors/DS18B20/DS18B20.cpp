c8b3a5e813d098b40d20f375af79bdd5d03a721f
ksackvil <sackvillehiik@gmail.com>
2023-01-13 21:27:07 -0800

    DS18B20 Error handling added

--- src/Sensors/DS18B20/DS18B20.cpp
+++ src/Sensors/DS18B20/DS18B20.cpp
@@ -1,13 +1,12 @@
 #include "DS18B20.hpp"
 
+void DS18B20::begin() {
+  ds18b20.begin();
+}
 
 Array<float> DS18B20::read() {
   Array<float> reading;
   ds18b20.requestTemperatures(); 
   reading.insert(ds18b20.getTempCByIndex(0));
   return reading;
-}
-
-void DS18B20::begin() {
-  ds18b20.begin();
 }
--- src/Sensors/DS18B20/DS18B20.hpp
+++ src/Sensors/DS18B20/DS18B20.hpp
@@ -5,18 +5,18 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-
 class DS18B20: public Sensor {
- private:
-  OneWire oneWire;	
-  DallasTemperature ds18b20;
-  Array<float> read();
-
  public:
   DS18B20(char inBaseDID, unsigned long inPollingFrequency, int inSignalPin):
     Sensor(inBaseDID, inPollingFrequency), oneWire(inSignalPin), ds18b20(&oneWire) {};
 
   void begin();
+
+ private:
+  Array<float> read();
+  
+  OneWire oneWire;	
+  DallasTemperature ds18b20;
 };
 
 #endif
